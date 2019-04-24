2f5ee160fd886083c3794c6ca2b9c2b0e719763e
Ritvik Bhawnani <ritvikbhawnani@gmail.com>
2019-04-24 17:18:02 -0500

    Minor changes to HomeFragment and StoresFragment

--- app/src/main/java/com/example/android/ediblelandscapes/HomeFragment.java
+++ app/src/main/java/com/example/android/ediblelandscapes/HomeFragment.java
@@ -21,18 +21,18 @@ public class HomeFragment extends Fragment {
 
         // list of store cards
         final ArrayList<Store> cards = new ArrayList<>();
-        cards.add(new Store("Fresh", "W. Johnson", R.mipmap.ic_launcher));
-        cards.add(new Store("Whole Foods", "S. University Avenue", R.mipmap.ic_launcher));
-        cards.add(new Store("Target", "W. Mills Street", R.mipmap.ic_launcher));
-        cards.add(new Store("Fresh", "W. Johnson", R.mipmap.ic_launcher));
-        cards.add(new Store("Whole Foods", "S. University Avenue", R.mipmap.ic_launcher));
-        cards.add(new Store("Target", "W. Mills Street", R.mipmap.ic_launcher));
-        cards.add(new Store("Fresh", "W. Johnson", R.mipmap.ic_launcher));
-        cards.add(new Store("Whole Foods", "S. University Avenue", R.mipmap.ic_launcher));
-        cards.add(new Store("Target", "W. Mills Street", R.mipmap.ic_launcher));
-        cards.add(new Store("Fresh", "W. Johnson", R.mipmap.ic_launcher));
-        cards.add(new Store("Whole Foods", "S. University Avenue", R.mipmap.ic_launcher));
-        cards.add(new Store("Target", "W. Mills Street", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 1", "Address 1", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 2", "Address 2", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 3", "Address 3", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 4", "Address 4", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 5", "Address 5", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 6", "Address 6", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 7", "Address 7", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 8", "Address 8", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 9", "Address 9", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 10", "Address 10", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 11", "Address 11", R.mipmap.ic_launcher));
+        cards.add(new Store("Store 12", "Address 12", R.mipmap.ic_launcher));
 
         StoreCardAdapter storeCardAdapter = new StoreCardAdapter(getActivity(), cards);
         ListView listView = rootView.findViewById(R.id.list);
